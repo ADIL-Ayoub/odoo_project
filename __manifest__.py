ac1ac609bc7d996a3597291ddca93d852aba3a79
Mohammed Aziz <64214635+MohammedAziz02@users.noreply.github.com>
2023-12-16 16:06:06 +0100

    adding access right

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,7 @@
 {
     'name': 'Abscence',
-    'data': ['views/professeur_view.xml', 'views/matiere_view.xml',
+    'data': [
+        'security/ir.model.access.csv','views/professeur_view.xml', 'views/matiere_view.xml',
              'views/filiere_view.xml', 'views/etudiant_view.xml', 'views/attendance_view.xml'
              ]
 }
