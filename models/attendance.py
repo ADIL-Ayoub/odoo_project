275b68704e8ed2a9eb3d5290a6f6fc0df4da4972
ADIL Ayoub <samih287@gmail.com>
2023-12-16 16:00:54 +0100

    e

--- models/attendance.py
+++ models/attendance.py
@@ -9,7 +9,8 @@ class Attendance(models.Model):
     libelle = fields.Char(string='Libelle', required=True)
     professor_id = fields.Many2one(
         'abscence.professeur', string='Professeur')
-    subject_ids = fields.Many2many('abscence.matiere', string='Matieres')
+    subject_ids = fields.Many2many(
+        'abscence.matiere', string='Matieres', compute="_compute_subjects")
 
     @api.depends('professor_id')
     def _compute_subjects(self):
@@ -19,8 +20,21 @@ class Attendance(models.Model):
         for record in self:
             if record.professor_id:
                 # Update the domain for the subject_ids field
-                record.subject_ids = [
-                    (6, 0, record.professor_id.matieres)]
+                record.subject_ids = record.professor_id.matieres
             else:
                 # Clear the subjects if no professor is selected
                 record.subject_ids = [(5, 0, 0)]
+
+            # if record.professor_id:
+            #     # Set the domain for the product_id field based on the selected customer
+            #     domain = [('id', 'in', record.professor_id.matieres)]
+            #     # Retrieve the first product related to the customer (if any)
+            #     first_subject = record.professor_id.matieres[:1]
+            #     # Set the product_id field with the first product (if any)
+            #     record.subject_ids = first_subject
+            #     # Set the domain for the product_id field
+            #     record.fields_get(['subject_ids'])[
+            #         'subject_ids']['domain'] = domain
+            # else:
+            #     # Clear the product_id if no customer is selected
+            #     record.subject_ids = False
